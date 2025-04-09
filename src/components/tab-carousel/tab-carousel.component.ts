fe997524a2ca0c726a57d2dd2b3a0cc49d2ae700
Kuladeep <kuladeepu.ec@conscendo.io>
2025-04-09 12:01:14 +0530

    added ngx-owl-carousel-o

--- src/components/tab-carousel/tab-carousel.component.ts
+++ src/components/tab-carousel/tab-carousel.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-tab-carousel',
-  imports: [],
+  imports: [CarouselModule], // Import OwlModule here
   templateUrl: './tab-carousel.component.html',
-  styleUrl: './tab-carousel.component.scss'
+  styleUrl: './tab-carousel.component.scss',
 })
 export class TabCarouselComponent {
-
+  customOptions: OwlOptions = {
+    mouseDrag: true, // Enable mouse dragging
+    touchDrag: true, // Enable touch dragging
+    pullDrag: true,
+    dots: true, // Show navigation dots
+    navSpeed: 700, // Transition speed
+    navText: ['Previous', 'Next'], // Custom nav button text
+    nav: true, // Show navigation arrows
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
 
-register(); // Register Swiper custom elements
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
