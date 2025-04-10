df208b7c6d52c1b3c5af242d1214e65a6442979c
vijayconscendo <vijayconscendo@gmail.com>
2025-04-10 17:48:15 +0530

    history and statements

--- src/components/icon-text-carousel/icon-text-carousel.component.ts
+++ src/components/icon-text-carousel/icon-text-carousel.component.ts
@@ -1,39 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-icon-text-carousel',
-  imports: [CommonModule, CarouselModule],
+  imports: [CommonModule],
   templateUrl: './icon-text-carousel.component.html',
   styleUrl: './icon-text-carousel.component.scss'
 })
 export class IconTextCarouselComponent {
-customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: true,
-    nav: true,
+// customOptions: OwlOptions = {
+//     loop: true,
+//     mouseDrag: true,
+//     touchDrag: true,
+//     pullDrag: false,
+//     dots: true,
+//     nav: true,
 
-    navSpeed: 700,
-    navText: ['owl-prev', 'owl-next'],
-    margin: 16,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-  }
+//     navSpeed: 700,
+//     navText: ['owl-prev', 'owl-next'],
+//     margin: 16,
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       400: {
+//         items: 2
+//       },
+//       740: {
+//         items: 3
+//       },
+//       940: {
+//         items: 4
+//       }
+//     },
+//   }
   
 }
