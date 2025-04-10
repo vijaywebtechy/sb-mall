2c96b918a8fa6f01aa6f4d60cc0d49d280e8aba4
Kuladeep <kuladeepu.ec@conscendo.io>
2025-04-10 17:31:57 +0530

    swiper-test

--- src/components/tab-carousel/tab-carousel.component.ts
+++ src/components/tab-carousel/tab-carousel.component.ts
@@ -1,25 +1,47 @@
-import { Component } from '@angular/core';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import {
+  Component,
+  OnInit,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { register } from 'swiper/element/bundle';
+
+// Register Swiper custom elements
+register();
 
 @Component({
   selector: 'app-tab-carousel',
-  imports: [CarouselModule], // Import OwlModule here
+  imports: [CommonModule], // Import OwlModule here
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './tab-carousel.component.html',
   styleUrl: './tab-carousel.component.scss',
 })
-export class TabCarouselComponent {
-  customOptions: OwlOptions = {
-    mouseDrag: true, // Enable mouse dragging
-    touchDrag: true, // Enable touch dragging
-    pullDrag: true,
-    dots: true, // Show navigation dots
-    navSpeed: 700, // Transition speed
-    navText: ['Previous', 'Next'], // Custom nav button text
-    nav: true, // Show navigation arrows
-    responsive: {
-      0: {
-        items: 1,
-      },
-    },
-  };
+export class TabCarouselComponent implements AfterViewInit {
+  @ViewChild('swiper') swiperContainer!: ElementRef;
+
+  slides = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'];
+  slidesPerView = 2;
+
+  ngAfterViewInit() {
+    const swiperEl = this.swiperContainer.nativeElement;
+
+    Object.assign(swiperEl, {
+      slidesPerView: this.slidesPerView,
+      spaceBetween: 30,
+    });
+
+    swiperEl.initialize();
+  }
+
+  // Optional: Add manual navigation methods
+  slideNext() {
+    this.swiperContainer.nativeElement.swiper.slideNext();
+  }
+
+  slidePrev() {
+    this.swiperContainer.nativeElement.swiper.slidePrev();
+  }
 }
